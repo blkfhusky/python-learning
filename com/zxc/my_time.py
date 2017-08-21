79fcc9cd9a867de216937a850552258a699fe4eb
zhuxc <zhuxc@joyowo.com>
2017-08-21 11:45:02 +0800

    python 引入模块

---
+++ com/zxc/my_time.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3     
+# -*- coding: utf-8 -*-  
+# create by zhuxc@joyowo.com
+# 2017/8/21
+
+import time
+import calendar
+import sys
+import my_func as mf
+# print(time.time())
+
+# t = time.asctime(time.localtime(time.time()))
+
+# print(t, "=====")
+
+# print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+
+# cal = calendar.month(2017, 1)
+# print(cal)
+print("iiiiiii")
+
+# print(sys.path)
+print(mf.area_of_cycle(11))
